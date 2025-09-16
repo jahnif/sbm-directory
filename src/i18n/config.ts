97173811e7b65938bbaab7439bbd3abcf7b4c027
jahnif <joseph.peha@gmail.com>
2025-09-16 22:21:00 +0200

    Fixed build errors v3

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,7 +1,2 @@
-import {createSharedPathnamesNavigation} from 'next-intl/navigation';
- 
 export const locales = ['en', 'es'] as const;
 export const defaultLocale = 'en' as const;
- 
-export const {Link, redirect, usePathname, useRouter} =
-  createSharedPathnamesNavigation({locales});
